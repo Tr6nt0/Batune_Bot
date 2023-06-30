37910b656b8b6dd5ad9b462aba2ac965b94ed404
kevmuri <kevmuri@umich.edu>
2023-06-30 15:20:05 -0400

    first pass at rewriting with sqlite functionality

--- mika.py
+++ mika.py
@@ -1,76 +1,79 @@
-# mika.py
-
-import os, discord, random, re
-from discord.ext import tasks
-from dotenv import load_dotenv
+import discord
+import re
+import sqlite3
+import sys
 from datetime import datetime
-from pathlib import Path
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+from discord.ext import tasks
+
+import CONFIG
 
 client = discord.Client()
+channel = client.get_channel(CONFIG.TARGET_CHANNEL)
 
-#ID of channel where questions are posted
-target = #fill in
+conn = sqlite3.connect('questions.db')
+cursor = conn.cursor()
 
-#Hour QOTD is to be posted
-posttime = #fill in
+if __name__ == '__main__':
+    if sys.argv[1] == '--reset-questions':
+        cursor.execute('DROP table IF EXISTS questions')
+        cursor.execute('CREATE table QUESTIONS (QUESTION TEXT)')
+        conn.commit()
+        conn.close()
+        print('Question Table Reset Successfully.')
+        exit()
 
-questionsfile = Path(__file__).with_name('questions.txt')
-tempfile = Path(__file__).with_name('temp.txt')
+    cursor.execute('CREATE table IF NOT EXISTS QUESTIONS (QUESTION TEXT)')
+    conn.commit()
+    conn.close()
 
-#Adds a question to the text file when called.
-def question_add(question):
-    with open(questionsfile, 'a') as questions:
-        questions.write('\n' + question)
+    client.run(CONFIG.DISCORD_TOKEN)
 
-def remove_question(qotd):
-    with open (questionsfile, "r") as input:
-        with open (tempfile, "w") as output:
-            for line in input:
-                if line.strip("\n") != qotd:
-                    output.write(line)
-    os.replace(tempfile, questionsfile)
 
-#Posts question of the day when called.
-async def question_post(channel):
-    with open(questionsfile, 'r') as questions:
-        qlines = questions.read().splitlines()
-        try:
-            qotd = random.choice(qlines)
-            await channel.send('QOTD: '+ qotd)
-            remove_question(qotd)
-        except:
-            await channel.send("No questions left. Everyone submit one!")
-    print(f'{client.user} has posted a qotd!')
+def add_question(question):
+    cursor.execute('INSERT INTO QUESTIONS VALUES (?)', question)
+    conn.commit()
+    conn.close()
 
-#Scheduled to call question of the day.
-@tasks.loop(minutes=60)
+
+def remove_question(question):
+    cursor.execute('DELETE FROM QUESTION VALUES (?)', question)
+    conn.commit()
+    conn.close()
+
+
+async def post_question():
+    cursor.execute('SELECT QUESTION FROM QUESTIONS ORDER BY RAND() LIMIT 1')
+    qotd = cursor.fetchone()
+    if qotd:
+        await channel.send('QOTD: ' + qotd)
+    else:
+        await channel.send('No questions left. Everyone submit one!')
+    print(f'{client.user} has posted!')
+
+
+@tasks.loop(seconds=60)
 async def task():
-    if datetime.now().hour == posttime:
-        channel = client.get_channel(target)
-        await question_post(channel)
+    if datetime.now().hour is CONFIG.SCHEDULED_POST_HOUR and datetime.now().minute is CONFIG.SCHEDULED_POST_MINUTE:
+        await post_question()
 
-#Make sure bot is online.
+
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord! It is '+ str(datetime.utcnow()))
+    print(f'{client.user} has connected to Discord! It is ' + str(datetime.utcnow()))
     await task.start()
 
-#Reads for commands, which includes adding questions and testing
+
 @client.event
 async def on_message(message):
     if message.content.lower().startswith('mika add '):
         question = re.sub('mika add ', '', message.content, flags=re.IGNORECASE)
-        await message.channel.send('QOTD ADDED: '+ question)
-        question_add(question)
-    if message.content.startswith('mika test') and message.author.guild_permissions.kick_members == True:
-        channel = client.get_channel(target)
-        await question_post(channel)
-    if message.content.startswith('mika say') and message.author.guild_permissions.kick_members == True:
-        puppetmessage = re.sub('mika say ', '', message.content, flags=re.IGNORECASE)
-        channel = client.get_channel(target)
-        await channel.send(puppetmessage)
+        await channel.send('QOTD ADDED: ' + question)
+        add_question(question)
 
-client.run(TOKEN)
+    if message.content.lower().startswith('mika test') and message.author.guild_permissions.kick_members:
+        await post_question()
+
+    if message.content.lower().startswith('mika say') and message.author.guild_permissions.kick_members:
+        my_message = re.sub('mika say ', '', message.content, flags=re.IGNORECASE)
+        await channel.send(my_message)
---
+++ mika_old.py
@@ -0,0 +1,76 @@
+# mika_old.py
+
+import os, discord, random, re
+from discord.ext import tasks
+from dotenv import load_dotenv
+from datetime import datetime
+from pathlib import Path
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+#ID of channel where questions are posted
+target = #fill in
+
+#Hour QOTD is to be posted
+posttime = #fill in
+
+questionsfile = Path(__file__).with_name('questions.txt')
+tempfile = Path(__file__).with_name('temp.txt')
+
+#Adds a question to the text file when called.
+def question_add(question):
+    with open(questionsfile, 'a') as questions:
+        questions.write('\n' + question)
+
+def remove_question(qotd):
+    with open (questionsfile, "r") as input:
+        with open (tempfile, "w") as output:
+            for line in input:
+                if line.strip("\n") != qotd:
+                    output.write(line)
+    os.replace(tempfile, questionsfile)
+
+#Posts question of the day when called.
+async def question_post(channel):
+    with open(questionsfile, 'r') as questions:
+        qlines = questions.read().splitlines()
+        try:
+            qotd = random.choice(qlines)
+            await channel.send('QOTD: '+ qotd)
+            remove_question(qotd)
+        except:
+            await channel.send("No questions left. Everyone submit one!")
+    print(f'{client.user} has posted a qotd!')
+
+#Scheduled to call question of the day.
+@tasks.loop(minutes=60)
+async def task():
+    if datetime.now().hour == posttime:
+        channel = client.get_channel(target)
+        await question_post(channel)
+
+#Make sure bot is online.
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord! It is '+ str(datetime.utcnow()))
+    await task.start()
+
+#Reads for commands, which includes adding questions and testing
+@client.event
+async def on_message(message):
+    if message.content.lower().startswith('mika add '):
+        question = re.sub('mika add ', '', message.content, flags=re.IGNORECASE)
+        await message.channel.send('QOTD ADDED: '+ question)
+        question_add(question)
+    if message.content.startswith('mika test') and message.author.guild_permissions.kick_members == True:
+        channel = client.get_channel(target)
+        await question_post(channel)
+    if message.content.startswith('mika say') and message.author.guild_permissions.kick_members == True:
+        puppetmessage = re.sub('mika say ', '', message.content, flags=re.IGNORECASE)
+        channel = client.get_channel(target)
+        await channel.send(puppetmessage)
+
+client.run(TOKEN)
