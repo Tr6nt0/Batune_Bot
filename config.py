e54650371fab95bd907f74c5d604fe3e015bc678
Tr6nt0 <57883299+Tr6nt0@users.noreply.github.com>
2025-07-01 00:27:53 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# Discord Bot Token from https://discord.com/developers/applications
+DISCORD_TOKEN = "your_bot_token_here"
+
+# ID of the channel where fortunes should be posted
+TARGET_CHANNEL = 123456789012345678
+
+# Scheduled posting time in UTC (24-hour format)
+SCHEDULED_POST_HOUR = 14  # 2 PM UTC
+SCHEDULED_POST_MINUTE = 0
